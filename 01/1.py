c2c6f6bfa43dab9b9f05542cbab7a9f93f3ae3c3
Chingyen <124688629+chingyen06@users.noreply.github.com>
2024-11-27 20:11:24 +0800

    Update 1.py

--- 01/1.py
+++ 01/1.py
@@ -1,1 +1,10 @@
+name = input()
+ID = input()
+chinese = int(input())
+cs101 = int(input())
+program = int(input())
 
+print("Name:", name, sep='')
+print("ID:", ID, sep='')
+print("Average:", (chinese + cs101 + program) // 3, sep='')
+print("Total:", chinese + cs101 + program, sep='')
