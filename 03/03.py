8646c2a2ee8c2b1533a359543ebe53feca47208d
Chingyen <124688629+chingyen06@users.noreply.github.com>
2024-12-02 19:12:21 +0800

    Create 03.py

---
+++ 03/03.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+
+Sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("Sum:", "%.2f" %Sum, sep='')
+print("Difference:", "%.2f" %difference, sep='')
+print("Product:", "%.2f" %product, sep='')
+print("Quotient:", "%.2f" %quotient, sep='')
